fixed likelihood calculation mistake

.ipynb_checkpoints/autoencoder-checkpoint.py
@@ -8,7 +8,7 @@ def loglikelihood_single(log_event_rate_list, log_mesh_rate_list, T):
     We take the log likelihood for better computational performance
     log likelihood of a single event list. Needed when we hav event lists of different length
     '''
-    integral = 0.5 * (torch.sum(log_mesh_rate_list[1:]) + torch.sum(log_mesh_rate_list[:-1])) * T / (len(log_mesh_rate_list)-1)
+    integral = 0.5 * (torch.sum(torch.exp(log_mesh_rate_list[1:])) + torch.sum(torch.exp(log_mesh_rate_list[:-1]))) * T / (len(log_mesh_rate_list)-1)
     return torch.sum(log_event_rate_list) - integral
 
 def loglikelihood(log_event_rate_list, log_mesh_rate_list, T):
@@ -20,7 +20,7 @@ def loglikelihood(log_event_rate_list, log_mesh_rate_list, T):
         T: (B,)
     '''
     B, n_mesh = log_mesh_rate_list.shape
-    integral = 0.5 * (torch.sum(log_mesh_rate_list[:,1:], dim=1) + torch.sum(log_mesh_rate_list[:,:-1], dim=1)) * T / (n_mesh-1)   # (B,)
+    integral = 0.5 * (torch.sum(torch.exp(log_mesh_rate_list[:,1:]), dim=1) + torch.sum(torch.exp(log_mesh_rate_list[:,:-1]), dim=1)) * T / (n_mesh-1)   # (B,)
     return torch.mean(torch.sum(log_event_rate_list, dim=1) - integral)
     
 
@@ -102,7 +102,7 @@ class Autoencoder(pl.LightningModule):
         self.pe_size = pe_size
         self.output_size = output_size
         self.encoder = MLP(self.input_size, self.latent_size)
-        self.decoder = MLP(self.latent_size+self.pe_size, self.output_size, activation='leakyrelu')
+        self.decoder = MLP(self.latent_size+self.pe_size, self.output_size, activation='relu')
         self.code = NaiveEncoding()
         self.resolution = resolution
         self.lam = lam
@@ -110,7 +110,7 @@ class Autoencoder(pl.LightningModule):
     def encode(self, encoder_input):
         self.latent = self.encoder(encoder_input) # (B, latent_size)
         
-    def decode(self, coded_t_list): # et_list has shape (B, n, pe_size)
+    def decode(self, coded_t_list): # coded_t_list has shape (B, n, pe_size)
         '''
         Input: et_list, the positional encoded t_list with shape (B, n, pe_size)
         Output: log rate function values at those t, with shape (B, n)
@@ -137,7 +137,7 @@ class Autoencoder(pl.LightningModule):
         coded_mesh_t_list = self.code(T.unsqueeze(1) * torch.linspace(0, 1, self.resolution+1).unsqueeze(0))
         log_event_rate_list = self.decode(coded_event_t_list).squeeze()
         log_mesh_rate_list = self.decode(coded_mesh_t_list).squeeze()
-        loss = loglikelihood(log_event_rate_list, log_mesh_rate_list, T) + self.lam * torch.norm(self.latent, p=2)
+        loss = -loglikelihood(log_event_rate_list, log_mesh_rate_list, T) + self.lam * torch.norm(self.latent, p=2)
         self.log('train_loss', loss)
         return loss
     